75f23576f3767f59a5ecd06d35cfc302fc5bdc96
Nathalie Laenen <r0589993@student.thomasmore.be>
2017-02-23 16:53:40 +0100

    soortKamer wordt soortPrijs

--- application/models/TypeKamer_model.php
+++ application/models/TypeKamer_model.php
@@ -1,6 +1,6 @@
 <?php
 
-class TypeKamer_model extends CI_Model {
+class typeKamer_model extends CI_Model {
 
     function __construct() {
         parent::__construct();
@@ -8,28 +8,28 @@ class TypeKamer_model extends CI_Model {
 
     function get($id) {
         $this->db->where('id', $id);
-        $query = $this->db->get('typekamer');
+        $query = $this->db->get('typeKamer');
         return $query->row();                 // genereert een kamertype object
     }
 
     function getAll() {
-        $query = $this->db->get('typekamer');  // genereert SELECT * FROM persoon
+        $query = $this->db->get('typeKamer');  // genereert SELECT * FROM persoon
         return $query->result();             // een array met kamertype-objecten
     }
 
-    function insert($typekamer) {
-        $this->db->insert('typekamer', $prijs);
+    function insert($typeKamer) {
+        $this->db->insert('typeKamer', $prijs);
         return $this->db->insert_id();
     }
 
-    function update($typekamer) {
+    function update($typeKamer) {
         $this->db->where('id', $prijs->id);
-        $this->db->update('typekamer', $typekamer);
+        $this->db->update('typeKamer', $typeKamer);
     }
 
     function delete($id) {
         $this->db->where('id', $id);
-        $this->db->delete('typekamer');
+        $this->db->delete('typeKamer');
     }
 
 }
