ae006010f4acb03212d1f3ceac1837230db286c7
Ellen Peeters <elena.peeters.15@gmail.com>
2017-02-23 16:50:31 +0100

    menu aangepast

--- application/views/main_master.php
+++ application/views/main_master.php
@@ -36,30 +36,19 @@
 </head>
 
 <body>
+   <p>Hotel Kempenrust</p>
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar">iets</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
+           <ul class="nav navbar-nav">
+                <button>
+                    <?php echo anchor('/home/index', 'Home', 'class="btn btn-default"'); ?>
                 </button>
-                <a class="navbar-brand" href="#">Hotel Kempenrust</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <?php echo anchor('/home/index', 'Home', 'class="btn btn-default"'); ?>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+               <button>
+                   <?php echo anchor('/home/aanmelden', 'Aanmelden', 'class="btn btn-default"');?>
+               </button>
+            </ul>
         </div>
-        <!-- /.container -->
     </nav>
     
     <!-- Page Content -->
