b3df5a346fda36fcd77cab4b5e9139387c2f9646
Ellen Peeters <elena.peeters.15@gmail.com>
2017-02-23 16:36:32 +0100

    menu aangepast

--- application/views/main_master.php
+++ application/views/main_master.php
@@ -43,26 +43,17 @@
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
+                    <span class="icon-bar">iets</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Welkom!</a>
+                <a class="navbar-brand" href="#">Hotel Kempenrust</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="#">Bootstrap</a>
-                    </li>
-                    <li>
-                        <a href="#">jQuery</a>
-                    </li>
-                    <li>
-                        <a href="#">Ajax</a>
-                    </li>
-                    <li>
-                        <a href="#">JSON</a>
+                        <?php echo anchor('/home/index', 'Home', 'class="btn btn-default"'); ?>
                     </li>
                 </ul>
             </div>
