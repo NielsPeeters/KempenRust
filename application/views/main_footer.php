3ae2f94c8c01dacd8af93db148317302f104f0bd
Niels Peeters <r0631114@student.thomasmore.be>
2017-02-23 15:37:12 +0100

    Footer-file aangemaakt in main_master startpage is aangepast

---
+++ application/views/main_footer.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-lg-12">
+        <p>Auteur : <?php echo $auteur; ?></p>
+        <p>Hotel Kempenrust</p>
+        <p>Opdrachtgever: Kristine Mangelschots</p>
+        <p>Team 05</p>
+        <p>Laenen Nathalie - Peeters Ellen - Peeters Niels - Van de Voorde Tim - Van Dooren Wout</p>
+    </div>
+</div>
