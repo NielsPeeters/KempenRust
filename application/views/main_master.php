e0f107556f274331a5438ce48475cf6fa5cfa7af
Ellen Peeters <elena.peeters.15@gmail.com>
2017-02-23 16:32:40 +0100

    menu terug in master

--- application/views/main_master.php
+++ application/views/main_master.php
@@ -36,6 +36,41 @@
 </head>
 
 <body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Welkom!</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Bootstrap</a>
+                    </li>
+                    <li>
+                        <a href="#">jQuery</a>
+                    </li>
+                    <li>
+                        <a href="#">Ajax</a>
+                    </li>
+                    <li>
+                        <a href="#">JSON</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    
     <!-- Page Content -->
     <div class="container">
 
