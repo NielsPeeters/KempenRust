a167a831b539f6464fb4df324863e1dc50978ffc
Ellen Peeters <elena.peeters.15@gmail.com>
2017-02-23 15:49:05 +0100

    css bestanden toegevoegd en nav veranderd

--- application/views/main_master.php
+++ application/views/main_master.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Hotel Kempenrust</title>
+    <title>CodeIgniter & Bootstrap</title>
 
     <!-- Bootstrap Core CSS -->
     <?php echo stylesheet("bootstrap.css"); ?>
@@ -36,42 +36,6 @@
 </head>
 
 <body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Welkom!</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#"><?php anchor('/test/index','Test') ?></a>
-                    </li>
-                    <li>
-                        <a href="#">Menu</a>
-                    </li>
-                    <li>
-                        <a href="#">Menu</a>
-                    </li>
-                    <li>
-                        <a href="#">Menu</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
     <!-- Page Content -->
     <div class="container">
 
@@ -112,7 +76,20 @@
 
         <!-- Footer -->
         <footer>
+<<<<<<< HEAD
             <?php echo $footer; ?>
+=======
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Hotel Kempenrust</p>
+                    <p>Opdrachtgever: Kristine Mangelschots</p> 
+                    <p>Team 05</p>
+                    <p>
+                        Laenen Nathalie - Peeters Ellen - Peeters Niels - Van de Voorde Tim - Van Dooren Wout 
+                    </p>
+                </div>
+            </div>
+>>>>>>> css bestanden toegevoegd en nav veranderd
         </footer>
 
     </div>
