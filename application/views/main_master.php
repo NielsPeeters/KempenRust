309f81ca3b33962dd25ccc1f1e2f0296a4241fcd
Ellen Peeters <elena.peeters.15@gmail.com>
2017-02-23 16:01:26 +0100

    dubbele tekst eruit gehaald

--- application/views/main_master.php
+++ application/views/main_master.php
@@ -77,16 +77,6 @@
         <!-- Footer -->
         <footer>
             <?php echo $footer; ?>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Hotel Kempenrust</p>
-                    <p>Opdrachtgever: Kristine Mangelschots</p> 
-                    <p>Team 05</p>
-                    <p>
-                        Laenen Nathalie - Peeters Ellen - Peeters Niels - Van de Voorde Tim - Van Dooren Wout 
-                    </p>
-                </div>
-            </div>
         </footer>
     </div>
     <!-- /.container -->
