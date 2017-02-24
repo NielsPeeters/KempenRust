790a1625f096f96a179f9dfcb5d0161ca2941459
Niels Peeters <r0631114@student.thomasmore.be>
2017-02-24 11:32:19 +0100

    Base_url herfixen voor edu.php site. Zet deze op localhost voor lokaal via xampp te hosten!

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://eduphp.thomasmore.be/~tmk1617_pro05/tmk1617_pro05/';
-$config['base_url'] = 'http://localhost:80/project';
+$config['base_url'] = 'http://eduphp.thomasmore.be/~tmk1617_pro05/tmk1617_pro05/';
+//$config['base_url'] = 'http://localhost:80/project';
 
 
 /*
