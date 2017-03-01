983a68c9fb613ecac211280f7e41284ba6ef6523
Nathalie Laenen <r0589993@student.thomasmore.be>
2017-03-01 19:17:14 +0100

    Input validatie bij registreer & commentaar bij persoon_model

--- application/models/Persoon_model.php
+++ application/models/Persoon_model.php
@@ -4,43 +4,72 @@ class Persoon_model extends CI_Model {
 
     function __construct()
     {
+        /**
+        * standaard model constructor
+        */
         parent::__construct();
     }
 
     function get($id)
     {
-      $this->db->where('id', $id);
-      $query = $this->db->get('persoon');
-      return $query->row();                 // genereert een persoon object
+        /**
+        *Geeft het persoon object terug dat bij het id hoort.
+        *\param id het id van het te halen persoon object
+        *\return een persoon object
+        */
+        $this->db->where('id', $id);
+        $query = $this->db->get('persoon');
+        return $query->row();                 // genereert een persoon object
     }
 
     function getAll()
     {
+        /**
+        *Geeft een array terug met alle persoon objecten.
+        *\return een array met persoon objecten
+        */
         $query = $this->db->get('persoon');	 // genereert SELECT * FROM persoon
         return $query->result();             // een array met persoon-objecten
     }
 
     function insert($persoon)
     {
+        /**
+        *Insert een persoon object in de database.
+        *\param persoon een persoon object
+        *\return een persoon object
+        */
         $this->db->insert('persoon', $persoon);
         return $this->db->insert_id();
     }
 
     function update($persoon)
     {
+        /**
+        *Update een persoon object in de database.
+        *\param persoon een persoon object
+        */
         $this->db->where('id', $persoon->id);
         $this->db->update('persoon', $persoon);
     }
 
     function delete($id)
     {
+        /**
+        *Verwijderdt een persoon object uit de database.
+        *\param id het id van het te verwijderen persoon object
+        */
         $this->db->where('id', $id);
         $this->db->delete('persoon');
     }
 
       function emailVrij($email)
     {
-        // is email al dan niet aanwezig
+        /**
+        *Gaat na of het meegegeven email adres al in de database voorkomt.
+        *\param email het te controleren email adres
+        *\return true als de email vrij is, anders false
+        */
         $this->db->where('email', $email);
         $query = $this->db->get('persoon');
         if ($query->num_rows() == 0) {
@@ -52,6 +81,12 @@ class Persoon_model extends CI_Model {
 
     function getAccount($email, $password)
     {
+        /**
+        *Gaat na of er een persoon object bestaat met de meegegeven waarden.
+        *\param email het ingegeven email adres
+        *\param password het ingegeven wachtwoord.
+        *\return een persoon object als deze bestaat, anders false.
+        */
         $this->db->where('email', $email);
         $this->db->where('wachtwoord', $password);
         $query = $this->db->get('persoon');
--- application/views/map/registreer.php
+++ application/views/map/registreer.php
@@ -22,19 +22,19 @@ echo form_open('persoon/registreer', $attributes);
 
 <div class="form-group">
     <label for="naam" class="control-label">Naam</label>
-    <?php echo form_input(array('name' => 'naam', 'id' => 'naam', 'value' => $persoon->naam, 'class' => 'form-control', 'placeholder' => 'Naam', 'required' => 'required')); ?>
+    <?php echo form_input(array('pattern'=>'[a-zA-Z]+','name' => 'naam', 'id' => 'naam', 'value' => $persoon->naam, 'class' => 'form-control', 'placeholder' => 'Naam', 'required' => 'required')); ?>
     </br>
     <label for="voornaam" class="control-label">Voornaam</label>
-    <?php echo form_input(array('name' => 'voornaam', 'id' => 'voornaam', 'value' => $persoon->voornaam, 'class' => 'form-control', 'placeholder' => 'Voornaam', 'required' => 'required')); ?>
+    <?php echo form_input(array('pattern'=>'[a-zA-Z]+','name' => 'voornaam', 'id' => 'voornaam', 'value' => $persoon->voornaam, 'class' => 'form-control', 'placeholder' => 'Voornaam', 'required' => 'required')); ?>
     </br>
-    <label for="plaats" class="control-label">Gemeente</label>
-    <?php echo form_input(array('name' => 'gemeente', 'id' => 'gemeente', 'value' => $persoon->gemeente, 'class' => 'form-control', 'placeholder' => 'Gemeente', 'required' => 'required')); ?>
+    <label for="gemeente" class="control-label">Gemeente</label>
+    <?php echo form_input(array('pattern'=>'[a-zA-Z]+','name' => 'gemeente', 'id' => 'gemeente', 'value' => $persoon->gemeente, 'class' => 'form-control', 'placeholder' => 'Gemeente', 'required' => 'required')); ?>
     </br>
     <label for="postcode" class="control-label">Postcode</label>
-    <?php echo form_input(array('type' => 'number', 'name' => 'postcode', 'id' => 'postcode', 'value' => $persoon->postcode, 'class' => 'form-control', 'placeholder' => 'Postcode', 'required' => 'required')); ?>
+    <?php echo form_input(array('title'=>'Een postcode bestaat uit 4 cijfers.','pattern'=>'[0-9]{4}', 'name' => 'postcode', 'id' => 'postcode', 'value' => $persoon->postcode, 'class' => 'form-control', 'placeholder' => 'Postcode', 'required' => 'required')); ?>
     </br>
     <label for="straat" class="control-label">Straat</label>
-    <?php echo form_input(array('name' => 'straat', 'id' => 'straat', 'value' => $persoon->straat, 'class' => 'form-control', 'placeholder' => 'Straat', 'required' => 'required')); ?>
+    <?php echo form_input(array('pattern'=>'[a-zA-Z]+','name' => 'straat', 'id' => 'straat', 'value' => $persoon->straat, 'class' => 'form-control', 'placeholder' => 'Straat', 'required' => 'required')); ?>
     </br>
     <label for="huisnummer" class="control-label">Huisnummer</label>
     <?php echo form_input(array('type' => 'number', 'name' => 'huisnummer', 'id' => 'huisnummer', 'value' => $persoon->huisnummer, 'class' => 'form-control', 'placeholder' => 'Huisnummer', 'required' => 'required')); ?>
@@ -43,19 +43,19 @@ echo form_open('persoon/registreer', $attributes);
     <?php echo form_input(array('name' => 'bus', 'id' => 'bus', 'value' => $persoon->bus, 'class' => 'form-control', 'placeholder' => 'Bus')); ?>
     </br>
     <label for="telefoon" class="control-label">Telefoon</label>
-    <?php echo form_input(array('type' => 'number', 'name' => 'telefoon', 'id' => 'telefoon', 'value' => $persoon->telefoon, 'class' => 'form-control', 'placeholder' => 'Telefoon')); ?>
+    <?php echo form_input(array('pattern' => '^\+?[0-9]{10}', 'name' => 'telefoon', 'id' => 'telefoon', 'value' => $persoon->telefoon, 'class' => 'form-control', 'placeholder' => 'Telefoon')); ?>
     </br>
     <label for="email" class="control-label">Email</label>
-    <?php echo form_input(array('name' => 'email', 'id' => 'email', 'value' => $persoon->email, 'class' => 'form-control', 'placeholder' => 'Email', 'required' => 'required')); ?>
+    <?php echo form_input(array('type'=>'email','name' => 'email', 'id' => 'email', 'value' => $persoon->email, 'class' => 'form-control', 'placeholder' => 'Email', 'required' => 'required')); ?>
     </br>
     <div class="alert alert-danger" id="label">
         Dit email adres is al in gebruik!
     </div>
     <label for="wachtwoord" class="control-label">Wachtwoord</label>
-    <?php echo form_input(array('type' => 'password', 'name' => 'wachtwoord', 'id' => 'wachtwoord', 'value' => 'Wachtwoord', 'class' => 'form-control', 'placeholder' => 'Wachtwoord')); ?>
+    <?php echo form_input(array('type' => 'password', 'name' => 'wachtwoord', 'id' => 'wachtwoord','class' => 'form-control', 'placeholder' => 'Wachtwoord')); ?>
     </br>
     <label for="wachtwoord2" class="control-label">Wachtwoord herhalen</label>
-    <?php echo form_input(array('type' => 'password', 'name' => 'wachtwoord2', 'id' => 'wachtwoord2', 'value' => 'Wachtwoord', 'class' => 'form-control', 'placeholder' => 'Wachtwoord')); ?>
+    <?php echo form_input(array('type' => 'password', 'name' => 'wachtwoord2', 'id' => 'wachtwoord2', 'class' => 'form-control', 'placeholder' => 'Wachtwoord')); ?>
     </br>
     <?php echo form_input(array('type' => 'hidden', 'name' => 'id', 'id' => 'id', 'value' => $persoon->id, 'class' => 'form-control', 'placeholder' => 'id')) ?>
 
