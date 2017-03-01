3220448ec60890534aa623e355840e605f050637
Tim Van de Voorde <r0580911@student.thomasmore.be>
2017-03-01 17:50:19 +0100

    issue

--- application/controllers/Kamertype.php
+++ application/controllers/Kamertype.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class kamertype extends CI_Controller {
+class Kamertype extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
