971c5ac9614ceebbcddcd69597fb2717b27e162e
Tim Van de Voorde <r0580911@student.thomasmore.be>
2017-03-01 16:38:53 +0100

    kamertype beheren start

--- application/models/TypeKamer_model.php
+++ application/models/TypeKamer_model.php
@@ -1,6 +1,6 @@
 <?php
 
-class typeKamer_model extends CI_Model {
+class typekamer_model extends CI_Model {
 
     function __construct() {
         parent::__construct();
@@ -8,28 +8,28 @@ class typeKamer_model extends CI_Model {
 
     function get($id) {
         $this->db->where('id', $id);
-        $query = $this->db->get('typeKamer');
+        $query = $this->db->get('kamerType');
         return $query->row();                 // genereert een kamertype object
     }
 
     function getAll() {
-        $query = $this->db->get('typeKamer');  // genereert SELECT * FROM persoon
+        $query = $this->db->get('kamerType');  // genereert SELECT * FROM persoon
         return $query->result();             // een array met kamertype-objecten
     }
 
     function insert($typeKamer) {
-        $this->db->insert('typeKamer', $prijs);
+        $this->db->insert('kamerType', $prijs);
         return $this->db->insert_id();
     }
 
     function update($typeKamer) {
         $this->db->where('id', $prijs->id);
-        $this->db->update('typeKamer', $typeKamer);
+        $this->db->update('kamerType', $typeKamer);
     }
 
     function delete($id) {
         $this->db->where('id', $id);
-        $this->db->delete('typeKamer');
+        $this->db->delete('kamerType');
     }
 
 }
