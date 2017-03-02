7932d7537da932eb4e2579cdfc66d903f9925f02
Ellen Peeters <elena.peeters.15@gmail.com>
2017-03-02 15:11:53 +0100

    faciliteiten beheren

---
+++ application/views/admin/faciliteit/faciliteit_lijst.php
@@ -0,0 +1,22 @@
+<div class="table-responsive">
+<table class="table">
+    <thead>
+        <tr>                                
+            <th>Naam</th>
+            <th>Beheer</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($types as $type) { ?>
+            <tr>
+                <td><?php echo $type->naam ?></td>
+                <td>
+                    <button data-toggle="tooltip" data-placement="bottom" title="Wijzigen" class="wijzigKamertype glyphicon glyphicon-pencil btn btn-primary" data-id="<?php echo $type->id ?>"></button>
+                    <button data-toggle="tooltip" data-placement="bottom" title="Verwijderen" class="verwijderKamertype glyphicon glyphicon-trash btn btn-danger" data-id="<?php echo $type->id ?>"></button>                                 
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+</div>
+
