a1a801fb532aa39d12a6e9ae135a6a1f02b87cb3
Ellen Peeters <elena.peeters.15@gmail.com>
2017-03-09 15:03:18 +0100

    faciliteit beheren: bugs oplossen en annuleren knop aanpassen

--- application/views/admin/faciliteit/ajax_faciliteit.php
+++ application/views/admin/faciliteit/ajax_faciliteit.php
@@ -1,20 +1,14 @@
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.3/css/bootstrap-select.min.css">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<script>
- $(document).ready(function(){
-    $('.opslaan').click(function(e){
-        /**
-        *Bij een klik op de opslaan knop wordt het form gevalideert.
-        */
-        $('#myForm').validator();
-    });
- });
 
-
-</script>
 <?php echo javascript("validator.js");?>
 
-<form name='myform' id='JqAjaxForm'>
+<?php
+echo javascript("validator.js");
+$attributes = array('name' => 'myform','data-toggle'=>'validator','role'=>'form');
+echo form_open('faciliteit/schrijfFaciliteit', $attributes);
+?>
+
     <div class="form-group">
         <label for="naam" class="control-label">Naam</label>
         <?php echo form_input(array('type'=>'text','name' => 'naam', 'id' => 'naam', 'value' => $kamer->naam, 'class' => 'form-control', 'placeholder' => 'Naam', 'required' => 'required'));?>
@@ -30,11 +24,10 @@
 
     <div class="help-block with-errors"></div>
     
-    <?php echo anchor('/home/index', '<p  id="annuleren" class="btn btn-secondary">Annuleren</p>'); ?>
+    <button type="button" class="btn btn-secondary annuleren">Annuleren</button>
     <button type="button" data-id="' . $kamer->id . '" class="btn btn-warning verwijder">Verwijderen</button>
-   
     <button type="submit" data-id="' . $kamer->id . '" class="btn btn-primary opslaan">Opslaan</button>
 
 </div>
-</form>
+<?php echo form_close();?>
 
