9768a3cdab4e518db367e9d0fb9c9e406ecec8d9
Ellen Peeters <elena.peeters.15@gmail.com>
2017-03-09 16:42:18 +0100

    ajax kant menu beheren

--- application/views/admin/menu/ajax_menu.php
+++ application/views/admin/menu/ajax_menu.php
@@ -1,8 +1,30 @@
-<?php
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.3/css/bootstrap-select.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<?php echo javascript("validator.js");?>
+
+<?php
+echo javascript("validator.js");
+$attributes = array('name' => 'myform','data-toggle'=>'validator','role'=>'form');
+echo form_open('menu/schrijfMenu', $attributes);
+?>
+
+    <div class="form-group">
+        <label for="naam" class="control-label">Naam</label>
+        <?php echo form_input(array('data-match'=>'','type'=>'text','name' => 'naam','id' => 'naam', 'value' => $menu->naam, 'class' => 'form-control', 'placeholder' => 'Naam', 'required' => 'required'));?>
+        
+        
+    </div>
+    </br>
+
+    <?php echo form_input(array('type' => 'hidden', 'name' => 'id', 'id' => 'id', 'value' => $menu->id, 'class' => 'form-control', 'placeholder' => 'id')) ?>
+
+    <div class="help-block with-errors"></div>
+    
+    <button type="button" class="btn btn-secondary annuleren">Annuleren</button>
+    <button type="button" data-id="' . $faciliteit->id . '" class="btn btn-warning verwijder">Verwijderen</button>
+    <button type="submit" data-id="' . $faciliteit->id . '" class="btn btn-primary opslaan">Opslaan</button>
+
+</div>
+</form>
 
