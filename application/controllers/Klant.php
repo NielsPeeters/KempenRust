6dc6e45e1a3324aa3402ddf6e5f0c5a7741a5ab1
Ellen Peeters <elena.peeters.15@gmail.com>
2017-03-29 13:04:25 +0200

    controller

--- application/controllers/Klant.php
+++ application/controllers/Klant.php
@@ -19,10 +19,29 @@ class Klant extends CI_Controller {
         $data['user'] = $this->authex->getUserInfo();
         $user = $this->authex->getUserInfo();
         if($user->soort==1) {
+            $this->load->model('arrangement_model');
+            $data['arrangementen'] = $this->arrangement_model->getAllArrangementen();
+            
+            $this->load->model('pension_model');
+            $data['pensions'] = $this->pension_model->getAll();
+            
             $partials = array('navbar' => 'main_navbar', 'content' => 'klant/boeking_maken1', 'footer' => 'main_footer');
             $this->template->load('main_master', $partials, $data);
         } else {
             redirect("/home/index");
         }       
+    }
+    
+    public function arrangementGekozen() {
+        $data['title'] = 'Boeking maken';
+        $data['author'] = 'Peeters Ellen';
+        $data['user'] = $this->authex->getUserInfo();
+        $user = $this->authex->getUserInfo();
+        if($user->soort==1) {         
+            $partials = array('navbar' => 'main_navbar', 'content' => 'klant/boeking_maken2', 'footer' => 'main_footer');
+            $this->template->load('main_master', $partials, $data);
+        } else {
+            redirect("/home/index");
+        }      
     }
 }
