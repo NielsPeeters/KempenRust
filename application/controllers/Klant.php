98ab8679721524a85a6b81183dfab75d39b1df7f
Ellen Peeters <elena.peeters.15@gmail.com>
2017-03-31 17:39:47 +0200

    ga naar home wanneer annuleer

--- application/controllers/Klant.php
+++ application/controllers/Klant.php
@@ -218,5 +218,7 @@ class Klant extends CI_Controller {
         if($this->session->has_userdata('pensionId')){
             $this->session->unset_userdata('pensionId');
         }
+        
+        redirect("/home/index");
     }
 }
