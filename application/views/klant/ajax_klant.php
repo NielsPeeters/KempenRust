5aa9462db15652cb145bf547c74965ea32efd15c
Tim Van de Voorde <r0580911@student.thomasmore.be>
2017-04-26 15:35:06 +0200

    klantgegevens aanpassen

---
+++ application/views/klant/ajax_klant.php
@@ -0,0 +1,86 @@
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.3/css/bootstrap-select.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script>
+    $(document).ready(function () {
+        $('.pause').click(function (e) {
+            $('#myForm').validator();
+        });
+    });
+
+
+</script>
+<?php
+echo javascript("validator.js");
+$attributes = array('name' => 'myform','id'=>'JqAjaxForm', 'data-toggle'=>'validator','role'=>'form');
+echo form_open('klant/schrijfJSONObject', $attributes);
+?>
+<form name='myform' id='JqAjaxForm'>
+    <div class="form-group">
+        <label for="naam" class="control-label">Naam</label>
+        <?php echo form_input(array('type' => 'text', 'name' => 'naam', 'id' => 'naam', 'value' => $user->naam, 'class' => 'form-control', 'placeholder' => 'Naam', 'required' => 'required')); ?>
+    </div>
+
+    <div class="form-group">
+        <label for="voornaam" class="control-label">Voornaam</label>
+        <?php echo form_input(array('type' => 'text', 'name' => 'voornaam', 'id' => 'voornaam', 'value' => $user->voornaam, 'class' => 'form-control', 'placeholder' => 'Voornaam', 'required' => 'required')); ?>
+    </div>
+
+    <div class="form-group">
+        <label for="gemeente" class="control-label">Gemeente</label>
+        <?php echo form_input(array('type' => 'text', 'name' => 'gemeente', 'id' => 'gemeente', 'value' => $user->gemeente, 'class' => 'form-control', 'placeholder' => 'gemeente', 'required' => 'required')); ?>
+    </div>
+
+    <div class="form-group">
+        <label for="postcode" class="control-label">Postcode</label>
+        <?php echo form_input(array('title' => ' postcode bestaat uit 4 cijfers.', 'pattern' => '[0-9]{4}', 'name' => 'postcode', 'id' => 'postcode', 'value' => $user->postcode, 'class' => 'form-control', 'placeholder' => 'Postcode', 'required' => 'required')); ?>
+    </div>
+
+    <div class="form-group">
+        <label for="straat" class="control-label">Straat</label>
+        <?php echo form_input(array('name' => 'straat', 'id' => 'straat', 'value' => $user->straat, 'class' => 'form-control', 'placeholder' => 'Straat', 'required' => 'required')); ?>
+    </div>
+
+    <div class="form-group">
+        <label for="huisnummer" class="control-label">Huisnummer</label>
+        <?php echo form_input(array('type' => 'number', 'name' => 'huisnummer', 'id' => 'huisnummer', 'value' => $user->huisnummer, 'class' => 'form-control', 'placeholder' => 'Huisnummer', 'required' => 'required')); ?>
+    </div>
+
+    <div class="form-group">
+        <label for="bus" class="control-label">Bus</label>
+        <?php echo form_input(array('name' => 'bus', 'id' => 'bus', 'value' => $user->bus, 'class' => 'form-control', 'placeholder' => 'Bus')); ?>
+    </div>
+
+    <div class="form-group">
+        <label for="telefoon" class="control-label">Telefoon</label>
+        <?php echo form_input(array('pattern' => '^\+?[0-9]{10}', 'name' => 'telefoon', 'id' => 'telefoon', 'value' => $user->telefoon, 'class' => 'form-control', 'placeholder' => 'Telefoon')); ?>
+    </div>
+
+    <div class="form-group">
+        <label for="email" class="control-label">Email</label>
+        <?php echo form_input(array('data-error' => 'This email address is invalid', 'type' => 'email', 'name' => 'email', 'id' => 'email', 'value' => $user->email, 'class' => 'form-control', 'placeholder' => 'Email', 'required' => 'required', 'data-error' => 'Dit is geen correct email adres.')); ?>
+        <div class="help-block with-errors"></div>
+
+    </div>
+
+    <div class="form-group">
+        <label for="wachtwoord" class="control-label">Wachtwoord</label>
+        <div class="form-inline row">
+            <div class="form-group col-sm-12">
+                <input type="password" data-minlength="6" class="form-control" id="een" name="wachtwoord" placeholder="Wachtwoord">
+                <div class="help-block">Minimum 6 karakters.</div>
+            </div>
+            <div class="form-group col-sm-12">
+                <input type="password" class="form-control" id="wachtwoord" data-match="#een" name="wachtwoord" data-match-error="Deze wachtwoorden komen niet overeen!" placeholder="Herhaal wachtwoord">
+                <div class="help-block with-errors"></div>
+            </div>
+        </div>
+    </div>
+
+    <?php echo form_input(array('type' => 'hidden', 'name' => 'id', 'id' => 'id', 'value' => $user->id, 'class' => 'form-control', 'placeholder' => 'id')) ?>
+
+    <div class="help-block with-errors"></div>
+
+    <button type="button" class="btn btn-secondary annuleren">Annuleren</button>
+    <button type="submit" data-id="' . $type->id . '" class="btn btn-primary opslaan">Opslaan</button>
+</div>
+</form>
---
+++ application/views/klant/klant_beheren.php
@@ -0,0 +1,54 @@
+<script>
+
+    function attach_click() {
+        $(".opslaan").click(function () {
+            /**
+             *Bij het klikken op opslaan wordt het kamertype object opgeslagen
+             */
+            schrijfPersoon();
+        })
+    }
+
+    $(document).ready(function () {
+        $("#panel").hide();
+
+        $("#persoon").change(function () {
+            /**
+             *Bij het veranderen van de geselecteerde kamertype, veranderdt de info in het panel
+             */
+            haalPersoon($(this).val());
+        });
+    });
+</script>
+
+<div class="modal fade" id="verwijderfout" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Waarschuwing</h4>
+            </div>
+            <div class="modal-body">
+                <p>
+                    Je kan dit account niet verwijderen omdat er nog boekingen aan verbonden zijn.
+                </p>
+
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn">Oke</button>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+<?php
+echo "</tbody></table>";
+?>
+
+<p>
+    <a id="terug" class="btn btn-secondary" href="javascript:history.go(-1);">Terug</a>
+</p>
