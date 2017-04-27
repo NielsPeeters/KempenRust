e99307d026c731baca97eec91ee0e8eda2ef3d20
Ellen Peeters <elena.peeters.15@gmail.com>
2017-04-27 16:48:37 +0200

    demovideo

--- application/views/klant/help/help.php
+++ application/views/klant/help/help.php
@@ -6,15 +6,15 @@
     
     <p>
         In deze video wordt getoond hoe je een boeking moet maken. Klik eerst op het menu-item 'Boeking maken' bovenaan in het menu. 
-        <br />
+        <br /><br />
         Vul een begin- en einddatum in en kies ofwel een arrangement of de optie 'Geen arrangement' zodat je een pension kunt kiezen. Wanneer de begindatum voor de 
         huidige datum valt of wanneer de einddatum voor de begindatum valt, zullen gepaste foutmeldingen getoond worden. Ook wanneer je een data die op andere dagen 
         vallen dan de begin- en einddag van het gekozen arrangement, zullen gepaste foutmeldingen getoond worden. 
-        <br />
+        <br /><br />
         Daarna kun je kamers toevoegen aan de boeking door op de knop 'Voeg een kamer toe' te klikken. Vul in hoeveel personen van elk persoontype (0-3, 4-8, 9-11, 
         12+, ...) er in de kamer zullen verblijven, kies een kamertype en duid aan of u een specifieke voorkeur voor een kamer heeft. Indien u een voorkeur heeft, kies 
         een kamer uit de lijst. Klik daarna op 'Toevoegen'. Indien je alle kamers aan de boeking hebt toegevoegd, klik op 'Bevestig boeking'.
-        <br />
+        <br /><br />
         Een pagina met de bevestiging en alle details zal getoond worden en een mail met alle details zal naar je e-mailadres gestuurd worden.
     </p>
 </div>
