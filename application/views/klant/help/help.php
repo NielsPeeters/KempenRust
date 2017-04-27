c23a9902072210c79a435c93bd46897f6937f081
Ellen Peeters <elena.peeters.15@gmail.com>
2017-04-27 16:36:06 +0200

    demovideo

--- application/views/klant/help/help.php
+++ application/views/klant/help/help.php
@@ -1,4 +1,9 @@
 <div class="row">
+    <video width="400" controls>
+	<source src="<?php echo base_url() + "assets/videos/demo.mp4";?>" type="video/mp4"/>
+	Helaas...
+    </video>
+    
     <p><?php echo site_url();?></p>
     <p><?php echo base_url();?></p>
 </div>
