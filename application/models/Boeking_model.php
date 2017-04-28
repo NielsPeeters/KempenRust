bf165552521f1a17fe5a9bdc62f08818435143fc
Nathalie Laenen <r0589993@student.thomasmore.be>
2017-04-28 17:02:46 +0200

    boekingen beheren op datum sorteren

--- application/models/Boeking_model.php
+++ application/models/Boeking_model.php
@@ -61,9 +61,7 @@ class Boeking_model extends CI_Model {
         * geeft een array boeking object terug met alle geassocieerde eigenschappen
         * \return een array boeking objecten
         */
-       
-       $this->db->order_by('goedgekeurd', 'asc');
-        $this->db->order_by('tijdstip', 'desc');
+        $this->db->order_by('startDatum', 'desc');
         $query = $this->db->get('boeking');
         $boekingen = $query->result();
         
