6473fb5ea71894c9210a8b359ae2a5f6f337a47f
Nathalie Laenen <r0589993@student.thomasmore.be>
2017-05-10 13:18:24 +0200

    email

--- application/models/KamerBoeking_model.php
+++ application/models/KamerBoeking_model.php
@@ -97,9 +97,11 @@ class kamerBoeking_model extends CI_Model {
         $this->load->model('kamer_model');
         $this->load->model('kamerType_model');
         foreach($kamerboekingen as $kamerboeking){
-            $kamerboeking->kamer = $this->kamer_model->get($kamerBoeking->kamerId);
-            //$kamerboeking->type = $this->kamerType_model->get($kamerBoeking->kamer->kamerTypeId);
+            $kamer = $this->kamer_model->get($kamerBoeking->kamerId);
+            $kamerboeking->kamer = $kamer->naam;
+            
         }
+        
 
         return $kamerboekingen->result();
     }
