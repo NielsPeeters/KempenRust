5fd5f2749efec8a7374227d939b8a54950ea7660
Niels Peeters <r0631114@student.thomasmore.be>
2017-05-11 15:58:43 +0200

    Kortingen naar komma

--- application/controllers/TypePersoon.php
+++ application/controllers/TypePersoon.php
@@ -18,7 +18,7 @@ class TypePersoon extends CI_Controller{
          * Laadt de pagina waarop je typepersoonen kan beheren
          * geeft een array van typepersoon objecten mee
          */
-        $data['title'] = 'Persoontype\'s beheren';
+        $data['title'] = 'Kortingen beheren';
         $data['author'] = 'Peeters Niels';
         $data['user'] = $this->authex->getUserInfo();
         $user = $this->authex->getUserInfo();
