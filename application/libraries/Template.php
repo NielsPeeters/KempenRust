32ae51762cc75f4d66b4ccfa980621ca7f82e80a
Tim Van de Voorde <r0580911@student.thomasmore.be>
2017-05-11 17:13:17 +0200

    doxygen

--- application/libraries/Template.php
+++ application/libraries/Template.php
@@ -1,4 +1,8 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
 /**
  * Template Class
  *
@@ -17,9 +21,8 @@ class Template {
      *
      * @access    public
      */
-    function __construct()
-    {
-                /**
+    function __construct() {
+        /**
          * constructor
          */
         log_message('debug', "Template Class Initialized");
@@ -37,24 +40,21 @@ class Template {
      * @param    bool
      * @return   parsed view
      */
-    function load($template = '', $view = array(), $vars = array(), $return = FALSE)
-    {
-                /**
+    function load($template = '', $view = array(), $vars = array(), $return = FALSE) {
+        /**
          * template loader
-                 * \param $template template
-                 * \param $view view
-                 * \param $vars variabelen
-                 * $return return
+         * \param $template template
+         * \param $view view
+         * \param $vars variabelen
+         * \return return
          */
-        $this->CI =& get_instance();
+        $this->CI = & get_instance();
         $tpl = array();
 
         // Check for partials to load
-        if (count($view) > 0)
-        {
+        if (count($view) > 0) {
             // Load views into var array
-            foreach($view as $key => $file)
-            {
+            foreach ($view as $key => $file) {
                 $tpl[$key] = $this->CI->load->view($file, $vars, TRUE);
             }
             // Merge into vars array
@@ -64,6 +64,7 @@ class Template {
         // Load master template
         return $this->CI->load->view($template, $vars, $return);
     }
+
 }
 
 ?>
